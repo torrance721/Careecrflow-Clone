3d6978cb802289343eb9851325d97162724c1952
xiejiangtian <torrance.xie721@gmail.com>
2026-01-16 21:49:33 +0800

    Init 1

--- server/_core/context.ts
+++ server/_core/context.ts
@@ -1,6 +1,5 @@
 import type { CreateExpressContextOptions } from "@trpc/server/adapters/express";
 import type { User } from "../../drizzle/schema";
-import { sdk } from "./sdk";
 
 export type TrpcContext = {
   req: any;
@@ -11,18 +10,18 @@ export type TrpcContext = {
 export async function createContext(
   opts: CreateExpressContextOptions
 ): Promise<TrpcContext> {
-  let user: User | null = null;
-
-  try {
-    user = await sdk.authenticateRequest(opts.req);
-  } catch (error) {
-    // Authentication is optional for public procedures.
-    user = null;
-  }
+  // Authentication disabled - always return null user
+  // To enable authentication later, uncomment and configure:
+  // import { sdk } from "./sdk";
+  // try {
+  //   user = await sdk.authenticateRequest(opts.req);
+  // } catch (error) {
+  //   user = null;
+  // }
 
   return {
     req: opts.req,
     res: opts.res,
-    user,
+    user: null,
   };
 }
