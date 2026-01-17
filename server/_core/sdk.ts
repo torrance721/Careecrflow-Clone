6ba00d91fe15516de9ee0cbf612058007f539aa1
xiejiangtian <torrance.xie721@gmail.com>
2026-01-18 00:14:26 +0800

    Init 1

--- server/_core/sdk.ts
+++ server/_core/sdk.ts
@@ -29,11 +29,10 @@ const GET_USER_INFO_WITH_JWT_PATH = `/webdev.v1.WebDevAuthPublicService/GetUserI
 
 class OAuthService {
   constructor(private client: ReturnType<typeof axios.create>) {
-    console.log("[OAuth] Initialized with baseURL:", ENV.oAuthServerUrl);
-    if (!ENV.oAuthServerUrl) {
-      console.error(
-        "[OAuth] ERROR: OAUTH_SERVER_URL is not configured! Set OAUTH_SERVER_URL environment variable."
-      );
+    if (ENV.oAuthServerUrl) {
+      console.log("[OAuth] Initialized with baseURL:", ENV.oAuthServerUrl);
+    } else {
+      console.log("[OAuth] OAUTH_SERVER_URL not configured - OAuth features disabled");
     }
   }
 
